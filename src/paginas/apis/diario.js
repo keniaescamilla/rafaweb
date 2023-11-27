5037db271b7498c659284867255a79fb0f559995
keniaescamilla <kenyaescamill@gmail.com>
2023-11-26 19:21:55 -0500

    sctavoo

--- src/paginas/apis/diario.js
+++ src/paginas/apis/diario.js
@@ -37,6 +37,20 @@ const DiarioForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <h2 className='h2-diario'>Escribe Como Te Sientes </h2>
+      <p className='p-diario'>
+Por qué es importante expresar lo que sentimos?
+<br></br>
+Muchas veces nos preguntamos por qué es importante expresar lo que sentimos y no guardar esos sentimientos para nosotros mismos.
+
+Pero cuando comprendemos cómo beneficia nuestra salud mental el mostrar nuestras emociones se disipa cualquier duda.
+
+Sentir miedo, tristeza, euforia es completamente normal y generalmente esos sentimientos dirigen nuestras acciones.
+
+Además, exteriorizarlos es importante para el desarrollo de la inteligencia emocional, que nos ayuda a llevar de una mejor manera nuestra vida en los diferentes aspectos.
+Escribe lo que sientas
+Si quieres saber lo que sientes puedes comenzar por escribirlo para luego poder expresarlo.
+
+Además, te puede ayudar a ensayar la mejor forma de comunicarlo a los demás.</p>
       <br></br>
       <br></br>
       <br></br>
