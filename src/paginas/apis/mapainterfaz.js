db97c9cc99f33e4de96f26a94f13c651e63c3926
keniaescamilla <kenyaescamill@gmail.com>
2023-12-01 02:05:14 -0500

    ya sirve el MAPS

--- src/paginas/apis/mapainterfaz.js
+++ src/paginas/apis/mapainterfaz.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './maps.css';
+
 const MapaInterfaz = () => {
   const mapRef = useRef(null);
   const searchInputRef = useRef(null);
@@ -7,113 +8,171 @@ const MapaInterfaz = () => {
   const infoWindowRef = useRef(null);
   const [places, setPlaces] = useState([]);
 
-  useEffect(() => {
-    const initMap = () => {
-      
-        if (window.google && window.google.maps) {
-          const map = new window.google.maps.Map(mapRef.current, {
-            zoom: 8,
-            center: { lat: 21.154122, lng: -86.8386295 },
-          });
-
-          const input = searchInputRef.current;
-          const searchBox = new window.google.maps.places.SearchBox(input);
-
-          map.addListener('bounds_changed', () => {
-            searchBox.setBounds(map.getBounds());
-          });
-
-          searchBox.addListener('places_changed', () => {
-            const places = searchBox.getPlaces();
-
-            if (places.length === 0) {
-              return;
-            }
-
-            const bounds = new window.google.maps.LatLngBounds();
-            places.forEach((place) => {
-              if (place.geometry && place.geometry.location) {
-                bounds.extend(place.geometry.location);
-              }
-            });
-
-            map.fitBounds(bounds);
-            setPlaces(places); // Almacena los lugares en el estado 'places'
-          });
-
-          const defaultSearch = 'psicólogo';
-          input.value = defaultSearch;
-
-          const placesService = new window.google.maps.places.PlacesService(map);
-          placesService.textSearch({ query: defaultSearch }, (results, status, pagination) => {
-            if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-              setPlaces(results); // Almacena los resultados en el estado 'places'
-              results.forEach((place) => {
+  const initMap = () => {
+    let placesService;
+  
+    if (window.google && window.google.maps) {
+      const map = new window.google.maps.Map(mapRef.current, {
+        zoom: 8,
+        center: { lat: 21.154122, lng: -86.8386295 },
+      });
+  
+      const input = searchInputRef.current;
+      const searchBox = new window.google.maps.places.SearchBox(input);
+  
+      map.addListener('bounds_changed', () => {
+        searchBox.setBounds(map.getBounds());
+      });
+  
+      searchBox.addListener('places_changed', () => {
+        const places = searchBox.getPlaces();
+  
+        if (places.length === 0) {
+          return;
+        }
+  
+        // Limpiar marcadores anteriores
+        markersRef.current.forEach((marker) => {
+          marker.setMap(null);
+        });
+        markersRef.current = [];
+  
+        const bounds = new window.google.maps.LatLngBounds();
+        places.forEach((place) => {
+          if (place.geometry && place.geometry.location) {
+            bounds.extend(place.geometry.location);
+          }
+        });
+  
+        map.fitBounds(bounds);
+        setPlaces(places);
+  
+        // Crear nuevos marcadores
+        placesService = new window.google.maps.places.PlacesService(map);
+        places.forEach((place) => {
+          placesService.getDetails(
+            { placeId: place.place_id },
+            (result, status) => {
+              if (status === window.google.maps.places.PlacesServiceStatus.OK) {
                 const marker = new window.google.maps.Marker({
-                  position: place.geometry.location,
+                  position: result.geometry.location,
                   map: map,
-                  title: place.name,
+                  title: result.name,
                 });
                 markersRef.current.push(marker);
-              });
-
-              if (pagination.hasNextPage) {
-                pagination.nextPage();
+  
+                marker.addListener('click', () => {
+                  const content = `<strong>${result.name}</strong><br>${result.formatted_address}`;
+                  showInfoWindow(marker, content);
+                });
+              } else {
+                console.error('Error al obtener detalles del lugar:', status);
               }
-            } else {
-              console.error('Error al buscar lugares:', status);
             }
-          });
-
-          const showInfoWindow = (marker, content) => {
-            if (!infoWindowRef.current) {
-              infoWindowRef.current = new window.google.maps.InfoWindow();
-            }
-            infoWindowRef.current.setContent(content);
-            infoWindowRef.current.open(map, marker);
-          };
-
-          markersRef.current.forEach((marker, index) => {
-            marker.addListener('click', () => {
-              const place = places[index];
-              const content = `<strong>${place.name}</strong><br>${place.formatted_address}`;
-              showInfoWindow(marker, content);
+          );
+        });
+      });
+  
+      // Verificar si hay un término de búsqueda inicial
+      const defaultSearch = input.value.trim();
+      if (defaultSearch) {
+        placesService.textSearch({ query: defaultSearch }, (results, status, pagination) => {
+          if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+            setPlaces(results);
+            results.forEach((place) => {
+              const marker = new window.google.maps.Marker({
+                position: place.geometry.location,
+                map: map,
+                title: place.name,
+              });
+              markersRef.current.push(marker);
             });
-          });
-        } else {
-          console.error('Google Maps API is not loaded correctly.');
+  
+            if (pagination.hasNextPage) {
+              pagination.nextPage();
+            }
+          } else {
+            console.error('Error al buscar lugares:', status);
+          }
+        });
+      }
+  
+
+
+
+
+      
+      const showInfoWindow = (marker, content) => {
+        if (!infoWindowRef.current) {
+          infoWindowRef.current = new window.google.maps.InfoWindow();
         }
-    };
-
-    const loadGoogleMapsScript = () => {
-      const script = document.createElement('script');
-      const API_KEY = 'AIzaSyDpxqx49qpfduYShktgqPZtti8cHpubI2s'; // Replace with your Google Maps API key
-
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&callback=initMap&libraries=places`;
-      script.async = true;
-      script.defer = true;
-
-      window.initMap = initMap; 
-
-      document.body.appendChild(script);
-
-      return () => {
-        document.body.removeChild(script);
+        infoWindowRef.current.setContent(content);
+        infoWindowRef.current.open(map, marker);
       };
+  
+      markersRef.current.forEach((marker, index) => {
+        marker.addListener('click', () => {
+          const place = places[index];
+          const content = `<strong>${place.name}</strong><br>${place.formatted_address}`;
+          showInfoWindow(marker, content);
+        });
+      });
+    } else {
+      console.error('Google Maps API is not loaded correctly.');
+    }
+  };
+  
+
+  useEffect(() => {
+    const loadGoogleMapsScript = () => {
+      const API_KEY = 'AIzaSyDpxqx49qpfduYShktgqPZtti8cHpubI2s';
+
+      return new Promise((resolve, reject) => {
+        if (window.google && window.google.maps && window.google.maps.places) {
+          resolve();
+        } else {
+          const script = document.createElement('script');
+          script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
+          script.async = true;
+          script.defer = true;
+
+          script.addEventListener('load', () => {
+            if (window.google && window.google.maps && window.google.maps.places) {
+              resolve();
+            } else {
+              reject(new Error('Error loading Google Maps API.'));
+            }
+          });
+
+          script.addEventListener('error', () => {
+            reject(new Error('Error loading Google Maps API.'));
+          });
+
+          document.body.appendChild(script);
+        }
+      });
     };
 
-    loadGoogleMapsScript();
-
-  },[]);
+    loadGoogleMapsScript()
+      .then(() => {
+        if (!window.initMap) {
+          window.initMap = initMap; // Asignar initMap globalmente si no está definido
+        }
+        initMap();
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  }, []);
 
   return (
-    <div>
+    <div className='min-h-screen'>
       <input
         ref={searchInputRef}
         placeholder="Search location"
         style={{ width: '100%', marginBottom: '10px' }}
       />
-      <div ref={mapRef} style={{ width: '100%', height: '400px' }} />
+      <div ref={mapRef} style={{ width: '100%', height: '100vh' }} />
     </div>
   );
 };
